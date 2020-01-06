68f69e1ce48e73d5cf4b61e33a0b165f2d7c3be9
Raphael Albern <ralpha19@hotmail.com>
2020-01-06 20:16:04 +0000

    added client.js file and export connect function from client.js file

--- play.js
+++ play.js
@@ -1,27 +1,5 @@
-const net = require('net');
+const connect = require('./client');
 
-/**
- * Establishes connection with the game server
- */
+console.log("Connecting.....");
 
- const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  //interpret data as text
-  conn.setEncoding('utf8');
-
-  conn.on('connect', () => {
-    console.log('you ded cuz you idled!');
-  })
-
-  return conn;
- }
-
- console.log("Connecting.....");
-
-
-
- connect();
+connect();
